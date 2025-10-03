d3612f8dd7a715323af26853c73998b1d0a1bdf4
Garrett <garrettwvb@gmail.com>
2025-10-03 15:47:57 +1000

    Added VAE to modules.py

--- recognition/VQ-VAE-2-s4578267/modules.py
+++ recognition/VQ-VAE-2-s4578267/modules.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 class EncodeBlock(nn.Module):
 
@@ -18,7 +19,7 @@ class EncodeBlock(nn.Module):
 
 class Encoder(nn.Module):
 
-    def __init__(self, in_channels=1, out_channels=[64,128,256], kernel_size=3):
+    def __init__(self, in_channels=1, out_channels=[64,128,256], latent_dim=64, image_size=(256,256), kernel_size=3):
         '''
         Encoder component of a VAE.
 
@@ -34,13 +35,22 @@ class Encoder(nn.Module):
         for out_ch in out_channels:
             self.layers.append(EncodeBlock(in_channels=in_ch, out_channels=out_ch, kernel_size=kernel_size))
             in_ch = out_ch
+
+        self.layers.append(nn.Flatten())
+
+        h, w = image_size[0] // pow(2, len(out_channels)), image_size[1] // pow(2, len(out_channels))
+
+        self.fc_mu = nn.Linear(in_features=in_ch * h * w, out_features=latent_dim)
+        self.fc_logvar = nn.Linear(in_features=in_ch * h * w, out_features=latent_dim)
     
     def forward(self, input):
         '''
-        
         '''
-        return self.layers(input)
-
+        output = self.layers(input)
+        print(output.shape)
+        mu = self.fc_mu(output)
+        logvar = self.fc_logvar(output)
+        return mu, logvar
 
 class DecodeBlock(nn.Module):
 
@@ -63,7 +73,8 @@ class Decoder(nn.Module):
 
         self.layers = nn.Sequential(
             nn.Linear(in_features=latent_dim, out_features=out_channels[0] * feature_map[0] * feature_map[1]),
-            nn.LeakyReLU()
+            nn.LeakyReLU(),
+            nn.Unflatten(1, (out_channels[0], feature_map[0], feature_map[1]))
         )
 
         for i in range(len(out_channels)-1):
@@ -78,6 +89,47 @@ class Decoder(nn.Module):
         '''
         return self.layers(input)
 
+class VAE(nn.Module):
+
+    def __init__(self, in_channels=1, out_channels=[64,128,256], latent_dim=32, kernel_size=3):
+        super(VAE, self).__init__()
+
+        self.encoder = Encoder(in_channels=in_channels, out_channels=out_channels, latent_dim=latent_dim, kernel_size=kernel_size)
+
+        rev_out = out_channels
+        rev_out.reverse()
+
+        self.decoder = Decoder(latent_dim=latent_dim, out_channels=rev_out[1:], feature_map=(latent_dim,latent_dim), kernel_size=kernel_size)
+
+    def reparameterize(self, mu, logvar):
+        """
+        Sample from N(mu, var) using N(0,1)
+        """
+        if self.training:
+            std = torch.exp(0.5 * logvar)
+            eps = torch.randn_like(std)
+            return mu + eps * std
+        else:
+            return mu
+        
+    def forward(self, x):
+        mu, logvar = self.encoder(x)
+        z = self.reparameterize(mu,logvar)
+        reconstruction = self.decoder(z)
+        return reconstruction, mu, logvar
+    
+    def loss_function(self, reconstructed_x, x, mu, logvar, beta=1.0):
+        """
+        VAE loss = Reconstruction loss + KL divergence
+        beta = weight for KL divergence (beta-VAE)
+        """
+        binary_cross_entropy = F.binary_cross_entropy(reconstructed_x, x, reduction='sum')
+
+        KL_divergence_loss = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+
+        return binary_cross_entropy + beta * KL_divergence_loss, binary_cross_entropy, KL_divergence_loss
+
+
 class VectorQuantizer(nn.Module):
 
     def __init__(self, *args, **kwargs) -> None:
@@ -86,14 +138,21 @@ class VectorQuantizer(nn.Module):
 
 
 if __name__ == "__main__":
-    eb = EncodeBlock(1, 256)
-    print(eb)
+    #eb = EncodeBlock(1, 256)
+    #print(eb)
+    #ve = Encoder()
+    #print(ve)
+    #db = DecodeBlock(256, 128)
+    #print(db)
+    #vd = Decoder(64)
+    #print(vd)
 
-    ve = Encoder()
-    print(ve)
+    #test_data = torch.randn((1,1,256,256))
 
-    db = DecodeBlock(256, 128)
-    print(db)
+    #print(test_data.shape)
 
-    vd = Decoder(64)
-    print(vd)
+    vae = VAE()
+    print(vae)
+
+    #vae(test_data)
+    #print(test_data.shape)
