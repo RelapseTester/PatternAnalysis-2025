8917d8f19883360826342315232c8dd4ca650ac6
RelapseTester <garrettwvb@gmail.com>
2025-10-16 13:46:05 +1000

    Added docstrings to modules.py classes and methods VectorQuantize.forward() also returns the indices

--- recognition/VQ-VAE-s4578267/modules.py
+++ recognition/VQ-VAE-s4578267/modules.py
@@ -3,8 +3,22 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class EncodeBlock(nn.Module):
+    """
+    Basic Encode block used in an Encoder. Block constists of a Conv2d, BatchNorm2d, LeakyReLU, and MaxPool2d.
+    Halves the Height and Width of the input using the MaxPool2d
+    """
 
-    def __init__(self, in_channels, out_channels, kernel_size=1, stride=1, padding=1):
+    def __init__(self, in_channels: int, out_channels: int, kernel_size=1, stride=1, padding=1):
+        """
+        Initialises The EncodeBlock with the given parameters.
+
+        Args:
+            in_channels (int): input channels to the Conv2d
+            out_channels (int): output channels to the Conv2d
+            kernel_size: kernel_size of the Conv2d
+            stride: stride of the Conv2d
+            padding: padding of the Conv2d
+        """
         super(EncodeBlock, self).__init__()
 
         self.layer = nn.Sequential(
@@ -14,20 +28,32 @@ class EncodeBlock(nn.Module):
             nn.MaxPool2d(kernel_size=2, stride=2) # halve height and width
         )
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Applies this block to the input x.
+
+        Args:
+            x (torch.Tensor): input data to this block
+        
+        Returns:
+            torch.Tensor: result of the input data parsed by this block
+        """
         out = self.layer(x)
         return out
 
 class Encoder(nn.Module):
+    """
+    Encoder composed of multiple EncodeBlocks. Does NOT flatten output.
+    """
 
-    def __init__(self, in_channels=1, out_channels=[64,128,256], latent_dim=64, image_size=(256,256), kernel_size=3):
-        '''
-        Encoder component of a VAE.
+    def __init__(self, in_channels=1, out_channels=[64,128,256], kernel_size=3):
+        """
+        Initialises The Encoder with the given parameters. 
 
-        in_channels (int, default=1): initial input channels to encoder.
-        out_channels (list[int], default=[256,128,64]): output channels of each layer. len(out_channels) is used to determine number of layers to create.
+        in_channels (int): initial input channels to encoder.
+        out_channels (list[int]): output channels of each layer. len(out_channels) is used to determine number of layers to create.
         kernel_size (int | (int,int)), default=3): kernel size for all layers. 
-        '''
+        """
         super(Encoder, self).__init__()
 
         self.layers = nn.Sequential()
@@ -38,14 +64,36 @@ class Encoder(nn.Module):
             in_ch = out_ch
     
     def forward(self, x):
-        '''
-        '''
+        """
+        Applies all EncodeBlocks sequentially to the input x.
+
+        Args:
+            x (torch.Tensor): input data to this Encoder.
+        
+        Returns:
+            torch.Tensor: result of the input data parsed by this Encoder. Return value is NOT flattened
+        """
         output = self.layers(x)
         return output
 
 class DecodeBlock(nn.Module):
+    """
+    Basic Decode block used in a Decoder. Block constists of a ConvTranspose2d and LeakyReLU.
+    Doubles the Height and Width of the input.
+    """
 
-    def __init__(self, in_channels, out_channels, kernel_size=1, stride=1, padding=1, out_padding=1):
+    def __init__(self, in_channels: int, out_channels: int, kernel_size=1, stride=1, padding=1, out_padding=1):
+        """
+        Initialises The DecodeBlock with the given parameters.
+
+        Args:
+            in_channels (int): input channels to the ConvTranspose2d
+            out_channels (int): output channels to the ConvTranspose2d
+            kernel_size: kernel_size of the ConvTranspose2d
+            stride: stride of the ConvTranspose2d
+            padding: padding of the ConvTranspose2d
+            out_padding: output_padding of the ConvTranspose2d
+        """
         super(DecodeBlock, self).__init__()
 
         self.layer = nn.Sequential(
@@ -54,13 +102,32 @@ class DecodeBlock(nn.Module):
         )
 
     def forward(self, x):
-        out = self.layer(x)
-        return out
+        """
+        Applies this block to the input x.
+
+        Args:
+            x (torch.Tensor): input data to this block
+        
+        Returns:
+            torch.Tensor: result of the input data parsed by this block
+        """
+        output = self.layer(x)
+        return output
     
 
 class Decoder(nn.Module):
+    """
+    Decoder composed of multiple EncodeBlocks. Unflattening of inputs must be done before parsing to the Decoder.
+    A final layer with a ConvTranspose2d with out_channels=1 and a Sigmoid function is appended to the end of the sequence.
+    """
 
-    def __init__(self, out_channels=[256,128,64], latent_dim=64, image_size=(256,256), kernel_size=3):
+    def __init__(self, out_channels=[256,128,64], kernel_size=3):
+        """
+        Initialises The Decoder with the given parameters. 
+
+        out_channels (list[int]): output channels of each layer. len(out_channels) is used to determine number of layers to create.
+        kernel_size (int | (int,int)), default=3): kernel size for all layers. 
+        """
         super(Decoder, self).__init__()
 
         self.layers = nn.Sequential()
@@ -72,17 +139,38 @@ class Decoder(nn.Module):
         self.layers.append(nn.Sigmoid())
 
     def forward(self, x):
-        '''
+        """
+        Applies all DecodeBlocks sequentially to the input x.
+
+        Args:
+            x (torch.Tensor): input data to this Decoder. Input must NOT be flattened.
         
-        '''
+        Returns:
+            torch.Tensor: result of the input data parsed by this Decoder. Return value is in range [0,1)
+        """
         return self.layers(x)
 
 class VAE(nn.Module):
+    """
+    Vanilla Variational Auto-Encoder.
+    Uses the Encoder, Decoder, and two linear layers for the distribution.
+    """
 
     def __init__(self, in_channels=1, out_channels=[64,128,256], latent_dim=32, kernel_size=3, image_size=(256,128)):
+        """
+        Initialises the VAE and it's components.
+        The Encoder and Decoder have the same number of EncodeBlocks and DecodeBlocks respectively.
+
+        Args:
+            in_channels (int): input channels to the Encoder.
+            out_channels (list[int]): output channels of each layer. len(out_channels) is used to determine number of layers to create.
+            latent_dim (int): Size of the latent dimension used in the reparameterisation of the VAE encoding.
+            kernel_size: kernel_size for both the Encoder and Decoder.
+            image_size (int,int): Height, Width dimensions of the input image.
+        """
         super(VAE, self).__init__()
 
-        self.encoder = Encoder(in_channels=in_channels, out_channels=out_channels, latent_dim=latent_dim, kernel_size=kernel_size, image_size=image_size)
+        self.encoder = Encoder(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size)
 
         self.encoder.layers.append(nn.Flatten())
 
@@ -95,11 +183,19 @@ class VAE(nn.Module):
         rev_out = out_channels
         rev_out.reverse()
 
-        self.decoder = Decoder(latent_dim=latent_dim, out_channels=rev_out, kernel_size=kernel_size)
+        self.decoder = Decoder(out_channels=rev_out, kernel_size=kernel_size)
 
-    def reparameterize(self, mu, logvar):
+    def reparameterize(self, mu: torch.Tensor, logvar: torch.Tensor):
         """
-        Sample from N(mu, var) using N(0,1)
+        Performs the reparameterisation of the VAE latent space data.
+        Samples from N(mu, var) using N(0,1)
+
+        Args:
+            mu (torch.Tensor): Sample means calculated from the fc_mu layer.
+            logvar (torch.Tensor): Sample log variance calculated from the fc_logvar layer.
+
+        Returns:
+            torch.Tensor: During training, returns mu + torch.randn_like(std) * torch.exp(0.5 * logvar). Otherwise returns mu.
         """
         if self.training:
             std = torch.exp(0.5 * logvar)
@@ -108,7 +204,16 @@ class VAE(nn.Module):
         else:
             return mu
         
-    def forward(self, x):
+    def forward(self, x: torch.Tensor):
+        """
+        Parses the input through this VAE, returning the reconstruction, mu, logvar.
+
+        Args:
+            x (torch.Tensor): input data to this VAE.
+
+        Returns:
+            torch.Tensor: result of the input data parsed by this VAE. Return value is in range [0,1)
+        """
         output = self.encoder(x)
         mu = self.fc_mu(output)
         logvar = self.fc_logvar(output)
@@ -116,12 +221,22 @@ class VAE(nn.Module):
         reconstruction = self.decoder(z)
         return reconstruction, mu, logvar
     
-    def loss_function(self, reconstructed_x, x, mu, logvar, beta=1.0):
+    def loss_function(self, predicted: torch.Tensor, target: torch.Tensor, mu: torch.Tensor, logvar: torch.Tensor, beta=1.0):
         """
         VAE loss = Reconstruction loss + KL divergence
         beta = weight for KL divergence (beta-VAE)
+
+        Args:
+            predicted (torch.Tensor): Output from the VAE.
+            target (torch.Tensor): True value of the input to the VAE.
+            mu (torch.Tensor): Sample mean values generated by the input.
+            logvar (torch.Tensor): Sample variance values generated by the input.
+            beta (float): Weight for the KL divergence.
+
+        Returns:
+            (torch.Tensor, torch.Tensor, torch.Tensor): binary_cross_entropy + beta * KL_divergence_loss, binary_cross_entropy, KL_divergence_loss
         """
-        binary_cross_entropy = F.binary_cross_entropy(reconstructed_x, x, reduction='sum')
+        binary_cross_entropy = F.binary_cross_entropy(predicted, target, reduction='sum')
 
         KL_divergence_loss = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
 
@@ -129,8 +244,18 @@ class VAE(nn.Module):
 
 
 class VectorQuantize(nn.Module):
+    """
+    Vector Quantisation module for use in a VQ-VAE model.
+    """
 
     def __init__(self, num_embeds=2048, embed_dim=64):
+        """
+        Initialises the Vector Quantizer with the given parameters.
+
+        Args:
+            num_embeds (int): Code book size. The number of values in the embedding lookup table.
+            embed_dim (int): Dimensionality of each vector in the codebook.
+        """
         super(VectorQuantize, self).__init__()
 
         self.num_embeds = num_embeds
@@ -142,6 +267,13 @@ class VectorQuantize(nn.Module):
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """
+        Parses the input through this Vector Quantiser module, returning the quantized outputs and the indices used.
+
+        Args:
+            x (torch.Tensor): input data to this Vector Quantizer.
+
+        Returns:
+            (torch.Tensor, torch.Tensor): Quantized outputs using the codebook lookup table, and the indices of each embedding used.
         
         REF: Inspired by VectorQuantizer in https://github.com/LukeDitria/pytorch_tutorials/blob/main/section07_autoencoders/solutions/Pytorch3_VQVAE.ipynb
         """
@@ -172,26 +304,48 @@ class VectorQuantize(nn.Module):
         if self.training:
             quantized = x + (quantized - x).detach()
 
-        ## Return in original shape
-        #   (Batch_size, Channels, Height, Width)
-        return quantized.permute(0, 3, 1, 2).contiguous()
+        ## Return the quantised tensor with the same shape as the input, and the encoding indices
+        #   (Batch_size, Channels, Height, Width), (Batch_size * Height * Width, 1)
+        return quantized.permute(0, 3, 1, 2).contiguous(), nearest_indices
 
 class VQVAE(nn.Module):
+    """
+    Vector Quantized Variational Auto-Encoder.
+    Uses the Encoder, Decoder, and VectorQuantize modules.
+    """
 
-    def __init__(self, in_channels=1, out_channels=[64, 128, 256], latent_dim=32, kernel_size=3, image_size=(256, 128), num_embeds=64):
+    def __init__(self, in_channels=1, out_channels=[64, 128, 256], latent_dim=32, kernel_size=3, num_embeds=64):
+        """
+        Initialises the VQVAE with the given parameters.
+
+        Args:
+            in_channels (int): input channels to the Encoder.
+            out_channels (list[int]): output channels of each layer. len(out_channels) is used to determine number of layers to create.
+            latent_dim (int): Dimensionality of the Vector Quantized embeddings.
+            kernel_size: kernel_size for both the Encoder and Decoder.
+            num_embeds (int): Code book size for the VectorQuantize module. The number of values in the embedding lookup table.
+        """
         super(VQVAE, self).__init__()
 
-        self.encoder = Encoder(in_channels=in_channels, out_channels=out_channels, latent_dim=latent_dim, kernel_size=kernel_size, image_size=image_size)
+        self.encoder = Encoder(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size)
 
         self.vq = VectorQuantize(num_embeds, latent_dim)
 
         rev_out = out_channels
         rev_out.reverse()
-        self.decoder = Decoder(out_channels=rev_out, latent_dim=latent_dim, image_size=image_size, kernel_size=kernel_size)
+        self.decoder = Decoder(out_channels=rev_out, kernel_size=kernel_size)
 
     def loss_function(self, predicted, target, commit_loss=1.0):
         """
-        
+        Computes both the encoding loss and the quantize loss.
+
+        Args:
+            predicted (torch.Tensor): Output of the VQVAE.
+            target (torch.Tensor): True value of the input to the VQVAE.
+            commit_loss (float): coefficient of the encode loss.
+
+        Returns:
+            torch.Tensor: encode_loss * commit_loss + quantize_loss
         """
         encode_loss = F.mse_loss(predicted.detach(), target)
         quantize_loss = F.mse_loss(predicted, target.detach())
@@ -199,17 +353,25 @@ class VQVAE(nn.Module):
         return loss
 
     def forward(self, x):
+        """
+        Parses the input through this VQ-VAE, returning the reconstruction, quantized, and index Tensors.
 
+        Args:
+            x (torch.Tensor): input data to this VQ-VAE.
+
+        Returns:
+            (torch.Tensor, torch.Tensor, torch.Tensor): Reconstructed_x, quantized_x, indices_x
+        """
         # Encode
         encoding = self.encoder(x)
 
         # Vector Quantize
-        quantized = self.vq(encoding)
+        quantized, indices = self.vq(encoding)
 
         # Decode
         decoded = self.decoder(quantized)
 
-        return decoded
+        return decoded, quantized, indices
 
 # Testing
 if __name__ == "__main__":
